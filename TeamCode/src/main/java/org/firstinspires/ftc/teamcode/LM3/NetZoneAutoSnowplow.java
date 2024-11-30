19a85c43080f0af0bf3b9be3d7e7ca66be89b3b3
asinghal09 <94330788+asinghal09@users.noreply.github.com>
2024-11-30 12:36:29 -0500

    11-30-24

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LM3/NetZoneAutoSnowplow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LM3/NetZoneAutoSnowplow.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
 
 @Autonomous
 @Config
-public class NetZoneAuto2 extends LinearOpMode {
+public class NetZoneAutoSnowplow extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -37,29 +37,27 @@ public class NetZoneAuto2 extends LinearOpMode {
 
         Trajectory deliverOne = drive.trajectoryBuilder(toSubmersible.end())    //drives up to submersible
                 .forward(11.5)
+                //.addTemporalMarker(6.75, ()->{
+                    //slidesSubsystem.setSlidesJointPos(1000,6); //brings down arm to push onto chamber
+                //})
                 .build();
         Trajectory backUp = drive.trajectoryBuilder(deliverOne.end())       //backs up from sub after delivering
                 .back(5)
                 .build();
         Trajectory pickOne = drive.trajectoryBuilder(backUp.end())         //strafes to pick up 1st sample
-                .strafeTo(new Vector2d(50,48))
-                .addSpatialMarker(new Vector2d(21, 39), () -> {
+                .strafeTo(new Vector2d(50,39))
+                .addSpatialMarker(new Vector2d(20, 39), () -> {
                 slidesSubsystem.runArmToPos(325,1);
                 })
                 .build();
         Trajectory basketLineup = drive.trajectoryBuilder(pickOne.end()) //goes to basket
-                .lineToSplineHeading(new Pose2d(54, 50, Math.toRadians(50)))
+                .lineToSplineHeading(new Pose2d(52, 48, Math.toRadians(55)))
                 .build();
         Trajectory deliver = drive.trajectoryBuilder(basketLineup.end())
-                .forward(13)
+                .forward(11.5)
                 .build();
         Trajectory backUp2 = drive.trajectoryBuilder(deliver.end())
                 .back(12)
-                .build();
-
-
-        Trajectory turnToTwo = drive.trajectoryBuilder(backUp2.end())
-                .lineToSplineHeading(new Pose2d(50,48, Math.toRadians(-64)))
                 .build();
 
         Trajectory backToTwo = drive.trajectoryBuilder(backUp2.end())
@@ -79,12 +77,12 @@ public class NetZoneAuto2 extends LinearOpMode {
         Trajectory pickTwo = drive.trajectoryBuilder(backUp2.end().plus(new Pose2d(0,0,Math.toRadians(220))))
                 .lineToSplineHeading(new Pose2d(58.5, 39, Math.toRadians(270)))
                 .build();
-        Trajectory basketLineup2 = drive.trajectoryBuilder(turnToTwo.end()) //goes to basket
-                .lineToSplineHeading(new Pose2d(51, 51, Math.toRadians(45)))
-                .build();
+        Trajectory basketLineup2 = drive.trajectoryBuilder(strafeToTwo.end()) //goes to basket
+                .lineToSplineHeading(new Pose2d(52, 48, Math.toRadians(45)))
+                        .build();
 
         Trajectory deliver2 = drive.trajectoryBuilder(basketLineup2.end())
-                .forward(11)
+                .forward(9)
                 .build();
         Trajectory backUp3 = drive.trajectoryBuilder(deliver2.end())
                 .back(20)
@@ -94,10 +92,10 @@ public class NetZoneAuto2 extends LinearOpMode {
                 .build();
 
         Trajectory push3 = drive.trajectoryBuilder(backUp3.end())
-                .lineToSplineHeading(new Pose2d(63,18,Math.toRadians(180)))
+                .lineToSplineHeading(new Pose2d(60,20,Math.toRadians(180)))
                 .build();
         Trajectory strafe3In = drive.trajectoryBuilder(push3.end())
-                .strafeRight(38)
+                .strafeRight(30)
                 .build();
 
 
@@ -128,7 +126,7 @@ public class NetZoneAuto2 extends LinearOpMode {
             slidesSubsystem.update();
         }
         slidesSubsystem.clawOpen();
-        slidesSubsystem.setSlides(1500);
+        slidesSubsystem.setSlides(300);
 
         while(opModeIsActive() && timer.seconds() < 0.5){
             slidesSubsystem.update();
@@ -146,24 +144,17 @@ public class NetZoneAuto2 extends LinearOpMode {
         }
         slidesSubsystem.clawClose();
         slidesSubsystem.runArmToPos(1650,1);
-
         timer.reset();
-        while(opModeIsActive() && timer.seconds() < 0.1){
-            slidesSubsystem.update();
-        }
-        slidesSubsystem.setSlides(0);
-        timer.reset();
-        while(opModeIsActive() && timer.seconds() < 0.1){
+        while(opModeIsActive() && timer.seconds() < 0.2){
             slidesSubsystem.update();
         }
         drive.followTrajectory(basketLineup);
-
         timer.reset();
         while(opModeIsActive() && timer.seconds() < 0.15){
             slidesSubsystem.update();
         }
 
-        slidesSubsystem.setSlides(4100);
+        slidesSubsystem.setSlides(4150);
 
         timer.reset();
         while(opModeIsActive() && timer.seconds() < 1){
@@ -181,34 +172,22 @@ public class NetZoneAuto2 extends LinearOpMode {
             slidesSubsystem.update();
         }
         drive.followTrajectory(backUp2);
-        slidesSubsystem.setSlides(500);
-        slidesSubsystem.runArmToPos(450,0.4);
+        slidesSubsystem.setSlides(300);
+        slidesSubsystem.runArmToPos(300,0.4);
+        drive.followTrajectory(spline);
 
+        slidesSubsystem.runArmToPos(0,0.5);
         timer.reset();
-        while(opModeIsActive() && timer.seconds() < 0.25){
-            slidesSubsystem.update();
-        }
-        drive.followTrajectory(turnToTwo);
-        slidesSubsystem.setSlides(1775);
-        slidesSubsystem.runArmToPos(175,0.5);
-        timer.reset();
-        while(opModeIsActive() && timer.seconds() < 1.2){
+        while(opModeIsActive() && timer.seconds() < 1){
             slidesSubsystem.update();
         }
         slidesSubsystem.clawClose();
         timer.reset();
-        while(opModeIsActive() && timer.seconds() < 0.2){
+        while(opModeIsActive() && timer.seconds() < 0.1){
             slidesSubsystem.update();
         }
         slidesSubsystem.runArmToPos(1650,1);
-        slidesSubsystem.setSlides(0);
-
-        timer.reset();
-        while(opModeIsActive() && timer.seconds() < 0.5){
-            slidesSubsystem.update();
-        }
         drive.followTrajectory(basketLineup2);
-
         slidesSubsystem.setSlides(4100);
         timer.reset();
         while(opModeIsActive() && timer.seconds() < 1.3){
@@ -225,13 +204,10 @@ public class NetZoneAuto2 extends LinearOpMode {
             slidesSubsystem.update();
         }
         drive.followTrajectory(backUp3);
-        timer.reset();
-        while(opModeIsActive() && timer.seconds() < 0.25){
-            slidesSubsystem.update();
-        }
-        slidesSubsystem.setSlides(0);
+        slidesSubsystem.setSlides(300);
+        drive.followTrajectory(push3);
         slidesSubsystem.runArmToPos(0,0.5);
-
+        drive.followTrajectory(strafe3In);
 
 
         // Continuous update for SlidesSubsystem
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/StringAscentTesting.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.testing;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class StringAscentTesting extends LinearOpMode {
+    DcMotor ascent;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ascent = hardwareMap.get(DcMotor.class, "ascent");
+
+        waitForStart();
+        while (opModeIsActive()){
+            ascent.setPower(1);
+
+        }
+    }
+}
